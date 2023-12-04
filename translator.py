d0fbaa94d37f25da0e4e9dbf9a984ff1f2e9e261
doverh <doverheld@gmail.com>
2023-12-04 10:13:44 -0500

    Add subfolders

--- translator.py
+++ translator.py
@@ -3,13 +3,21 @@ import os
 from deep_translator import ChatGptTranslator
 
 #Read a file in language X and translates it to Language Y
-def translateFile(file):
-    translated = ChatGptTranslator(api_key=config.OPENAI_API_KEY, target='pt').translate_file(file)
+def translateFile(file,idiom):
+    file_translate = os.getcwd() + '/Translate/' + file
+    translated = ChatGptTranslator(api_key=config.OPENAI_API_KEY, target=idiom).translate_file(file_translate)
     file_name = os.path.splitext(file)[0]+'_translated.txt'
-    #print('File name: ', file_name)
-    translated_file = open(file_name, 'w')
-    translated_file.write(translated)
-    return translated_file
+    file_path = os.getcwd() + '/Translate/Translated/' + file_name
+    
+    # Check if file already exists
+    if os.path.exists(file_path):
+        print('file already exists')
+    else:
+    # create a new file
+        with open(file_path, 'w') as fp:
+            fp.write(translated)
+    
+    return file_path
 
 
-print(translateFile("text_translate.txt"))
+print(translateFile("README.md","pt"))
